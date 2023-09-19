74d8be18ede09e651194b0770f14cef127401671
Anisa Maharani <anisamhran88@gmail.com>
2023-09-19 21:17:10 +0700

    admin pre-event 1 (answer)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            PreeventQuestionSeeder::class
+            PreeventQuestionSeeder::class,
+            PreeventAnswerSeeder::class
         ]);
     }
 }
